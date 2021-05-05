5134a15620ded70a26ef9738707d2c7d9a813537
surajseth90 <surajseth90@gmail.com>
2021-05-05 11:31:50 +0530

    Refactor the code to check Test cases

---
+++ src/test/java/com/bridgelabz/findmaximumtest/FindMaximumTest.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.findmaximumtest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.findmaximum.FindMaximum;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+
+public class FindMaximumTest {
+	@Test
+	public void givenFirstInteger_whenMax_shouldReturnTrue() {
+		Integer max =20;
+		Assert.assertEquals(max,FindMaximum.findMax(20, 10, 5) );
+	}
+	@Test
+	public void givenSecondInteger_whenMax_shouldReturnTrue() {
+		Integer max =45;
+		Assert.assertEquals(max,FindMaximum.findMax(20, 45, 5) );
+	}
+	@Test
+	public void givenThirdInteger_whenMax_shouldReturnTrue() {
+		Integer max =50;
+		Assert.assertEquals(max,FindMaximum.findMax(20, 10, 50) );
+	}
+}
